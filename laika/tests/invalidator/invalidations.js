b2173b997a6f5a3e18f10bd51b801d6dd612d43f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-14 15:07:45 +0530

    added multiUpdate invalidation support

--- laika/tests/invalidator/invalidations.js
+++ laika/tests/invalidator/invalidations.js
@@ -252,4 +252,187 @@ suite('Invalidator - Invalidations', function() {
       done();
     });
   });
+
+  suite('multiUpdate', function() {
+    test('trigger changed', function(done, server) {
+      var error = server.evalSync(function() {
+        coll = new Meteor.SmartCollection('sss');
+        if(coll._collection) {
+          doInsert();
+        } else {
+          coll.once('ready', doInsert);
+        }
+
+        function doInsert() {
+          coll._collection.insert({_id: 123, aa: 10, bb: 20}, function(err) {
+            coll._collection.insert({_id: 124, aa: 10, bb: 30}, function(err) {
+              emit('return', err);
+            });
+          });
+        }
+      });
+      assert.equal(error, undefined);
+
+      var rtn = server.eval(function() {
+        var c1 = {
+          _added: function(doc) { emit('added', doc) },
+          _selectorMatcher: function() { return false; },
+          _idExists: function() { return false }
+        };
+
+        var c2 = {
+          _changed: function(id, fields) { emit('changed', id, fields); },
+          _selectorMatcher: function() { return true; },
+          _idExists: function() { return true }
+        };
+
+        Meteor.SmartInvalidator.registerCollection('sss', coll);
+        Meteor.SmartInvalidator.addCursor('sss', c1);
+        Meteor.SmartInvalidator.addCursor('sss', c2);
+
+        Meteor.SmartInvalidator.invalidateMultiUpdate('sss', {aa: 10}, {$inc: {bb: 10}});
+      });
+
+      server.on('added', function() {
+        assert.fail('should not add anything');
+      }); 
+
+      var results = {};
+      server.on('changed', function(id, fields) {
+        results[id] = fields;
+      });
+
+      setTimeout(function() {
+        assert.deepEqual(results, {
+          "123": {bb: 20},
+          "124": {bb: 30}
+        });
+        done();
+      }, 50);
+    });
+
+    test('trigger changed and added', function(done, server) {
+      var error = server.evalSync(function() {
+        coll = new Meteor.SmartCollection('sss');
+        if(coll._collection) {
+          doInsert();
+        } else {
+          coll.once('ready', doInsert);
+        }
+
+        function doInsert() {
+          coll._collection.insert({_id: 123, aa: 10, bb: 20}, function(err) {
+            coll._collection.insert({_id: 124, aa: 10, bb: 30}, function(err) {
+              emit('return', err);
+            });
+          });
+        }
+      });
+      assert.equal(error, undefined);
+
+      var rtn = server.eval(function() {
+        var c1 = {
+          _added: function(doc) { emit('added', doc) },
+          _selectorMatcher: function() { return true; },
+          _idExists: function() { return false }
+        };
+
+        var c2 = {
+          _changed: function(id, fields) { emit('changed', id, fields); },
+          _selectorMatcher: function() { return true; },
+          _idExists: function() { return true }
+        };
+
+        Meteor.SmartInvalidator.registerCollection('sss', coll);
+        Meteor.SmartInvalidator.addCursor('sss', c1);
+        Meteor.SmartInvalidator.addCursor('sss', c2);
+
+        Meteor.SmartInvalidator.invalidateMultiUpdate('sss', {aa: 10}, {$inc: {bb: 10}});
+      });
+
+      var added = {};
+      server.on('added', function(doc) {
+        added[doc._id] = doc;
+      }); 
+
+      var changed = {};
+      server.on('changed', function(id, fields) {
+        changed[id] = fields;
+      });
+
+      setTimeout(function() {
+        assert.deepEqual(added, {
+          "123": {_id: 123, aa: 10, bb: 20},
+          "124": {_id: 124, aa: 10, bb: 30}
+        });
+        assert.deepEqual(changed, {
+          "123": {bb: 20},
+          "124": {bb: 30}
+        });
+        done();
+      }, 50);
+    });
+
+    test('trigger changed and removed', function(done, server) {
+      var error = server.evalSync(function() {
+        coll = new Meteor.SmartCollection('sss');
+        if(coll._collection) {
+          doInsert();
+        } else {
+          coll.once('ready', doInsert);
+        }
+
+        function doInsert() {
+          coll._collection.insert({_id: 123, aa: 10, bb: 20}, function(err) {
+            coll._collection.insert({_id: 124, aa: 10, bb: 30}, function(err) {
+              emit('return', err);
+            });
+          });
+        }
+      });
+      assert.equal(error, undefined);
+
+      var rtn = server.eval(function() {
+        var removed = {};
+        var c1 = {
+          _removed: function(id) { emit('removed', id); removed[id] = true; },
+          _selectorMatcher: function() { return false; },
+          _idExists: function(id) { 
+            return !removed[id];
+          }
+        };
+
+        var c2 = {
+          _changed: function(id, fields) { emit('changed', id, fields); },
+          _selectorMatcher: function() { return true; },
+          _idExists: function() { return true }
+        };
+
+        Meteor.SmartInvalidator.registerCollection('sss', coll);
+        Meteor.SmartInvalidator.addCursor('sss', c1);
+        Meteor.SmartInvalidator.addCursor('sss', c2);
+
+        Meteor.SmartInvalidator.invalidateMultiUpdate('sss', {aa: 10}, {$inc: {bb: 10}});
+      });
+
+      var removed = [];
+      server.on('removed', function(id) {
+        removed.push(id);
+      }); 
+
+      var changed = {};
+      server.on('changed', function(id, fields) {
+        changed[id] = fields;
+      });
+
+      setTimeout(function() {
+        assert.deepEqual(removed, [123, 124]);
+        assert.deepEqual(changed, {
+          "123": {bb: 20},
+          "124": {bb: 30}
+        });
+        done();
+      }, 50);
+    });
+  });
 }); 
