1d009a03e9b45298564511352344704943bad085
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-25 22:11:28 +0530

    added tests

--- laika/tests/queue_invalidator.js
+++ laika/tests/queue_invalidator.js
@@ -88,4 +88,159 @@ suite('Queue Invalidator', function() {
     assert.deepEqual(results, ['id1']);
     done();
   });
+
+  test('multi update once', function(done) {
+    var results = [];
+    var qi = new Meteor.SmartQueueInvalidator({
+      multiUpdate: function(selector, fields, callback) {
+        results.push([selector, fields]);
+        setTimeout(callback, 0);
+      }
+    });
+    qi.multiUpdate({aa: 10}, {$set: {bb: 20}});
+
+    assert.equal(qi._globalProcessing, true);
+    assert.deepEqual(qi._globalQueue, []);
+    assert.equal(qi._globalHook, null);
+
+    setTimeout(function() {
+      assert.deepEqual(results, [[{aa: 10}, {bb: 1}]]);
+      assert.equal(qi._globalProcessing, false);
+      assert.deepEqual(qi._globalQueue, []);
+      assert.equal(qi._globalHook, null);
+      done();
+    }, 10);
+  });
+
+  test('multi update and multi remove once', function(done) {
+    var results = [];
+    var qi = new Meteor.SmartQueueInvalidator({
+      multiUpdate: function(selector, fields, callback) {
+        results.push([selector, fields]);
+        setTimeout(callback, 0);
+      },
+      multiRemove: function(selector, callback) {
+        results.push([selector]);
+        setTimeout(callback, 0);
+      }
+    });
+    qi.multiUpdate({aa: 10}, {$set: {bb: 20}});
+    qi.multiRemove({cc: 10});
+
+    assert.equal(qi._globalProcessing, true);
+    assert.deepEqual(qi._globalQueue, [['mr', {cc: 10}]]);
+    assert.equal(qi._globalHook, null);
+
+    setTimeout(function() {
+      assert.deepEqual(results, [[{aa: 10}, {bb: 1}], [{cc: 10}]]);
+      assert.equal(qi._globalProcessing, false);
+      assert.deepEqual(qi._globalQueue, []);
+      assert.equal(qi._globalHook, null);
+      done();
+    }, 10);
+  });
+
+  test('adding id update after while multi remove', function(done) {
+    var results = [];
+    var qi = new Meteor.SmartQueueInvalidator({
+      multiUpdate: function(selector, fields, callback) {
+        results.push([selector, fields]);
+        setTimeout(callback, 0);
+      },
+      update: function(id, fields, callback) {
+        results.push([id, fields]);
+        setTimeout(callback, 0);
+      }
+    });
+    qi.multiUpdate({aa: 10}, {$set: {bb: 20}});
+    qi.update('id1', {$set: {cc: 20}});
+
+    assert.deepEqual(qi._idQueues, {});
+    assert.equal(qi._globalProcessing, true);
+    assert.deepEqual(qi._globalQueue, [['u', 'id1', {$set: {cc: 20}}]]);
+    assert.equal(qi._globalHook, null);
+
+    setTimeout(function() {
+      assert.deepEqual(results, [[{aa: 10}, {bb: 1}], ['id1', {cc: 1}]]);
+      assert.equal(qi._globalProcessing, false);
+      assert.deepEqual(qi._globalQueue, []);
+      assert.equal(qi._globalHook, null);
+      assert.deepEqual(qi._idsProcessing, []);
+      assert.deepEqual(qi._idQueues, {id1: []});
+
+      done();
+    }, 10);
+  });
+
+  test('add id update, multi update at once', function(done) {
+    var results = [];
+    var qi = new Meteor.SmartQueueInvalidator({
+      multiUpdate: function(selector, fields, callback) {
+        results.push([selector, fields]);
+        setTimeout(callback, 0);
+      },
+      update: function(id, fields, callback) {
+        results.push([id, fields]);
+        setTimeout(callback, 0);
+      }
+    });
+    qi.update('id1', {$set: {cc: 20}});
+    qi.multiUpdate({aa: 10}, {$set: {bb: 20}});
+
+    assert.deepEqual(qi._idQueues, {id1: []});
+    assert.deepEqual(qi._idsProcessing, ['id1']);
+    assert.equal(qi._globalProcessing, false);
+    assert.deepEqual(qi._globalQueue, [['mu', {aa: 10}, {$set: {bb: 20}}]]);
+    assert.equal(typeof(qi._globalHook), 'function');
+
+    setTimeout(function() {
+      assert.deepEqual(results, [['id1', {cc: 1}], [{aa: 10}, {bb: 1}]]);
+      assert.equal(qi._globalProcessing, false);
+      assert.deepEqual(qi._globalQueue, []);
+      assert.equal(qi._globalHook, null);
+      assert.deepEqual(qi._idsProcessing, []);
+      assert.deepEqual(qi._idQueues, {id1: []});
+
+      done();
+    }, 10);
+  });
+
+  // test('add multi update, id update, multi remove at once', function(done) {
+  //   var results = [];
+  //   var qi = new Meteor.SmartQueueInvalidator({
+  //     multiUpdate: function(selector, fields, callback) {
+  //       results.push([selector, fields]);
+  //       setTimeout(callback, 0);
+  //     },
+  //     update: function(id, fields, callback) {
+  //       results.push([id, fields]);
+  //       callback();
+  //       assert.deepEqual(qi._globalProcessing, true);
+  //       assert.deepEqual(qi._globalQueue, []);
+  //     },
+  //     multiRemove: function(selector, callback) {
+  //       results.push([selector]);
+  //       setTimeout(callback, 0);
+  //     }
+  //   });
+
+  //   qi.multiUpdate({aa: 10}, {$set: {bb: 20}});
+  //   qi.update('id1', {$set: {aa: 30}});
+  //   qi.multiRemove({cc: 10});
+
+  //   assert.deepEqual(qi._globalProcessing, true);
+  //   assert.deepEqual(qi._globalQueue[
+  //     ['u', 'id1', {$set: {aa: 30}}],
+  //     ['mr', {cc: 10}]
+  //   ]);
+
+  //   setTimeout(function() {
+  //     assert.deepEqual(results, [
+  //       [{aa: 10}, {bb: 1}],
+  //       ['id1', {aa: 1}],
+  //       [{cc: 10}]
+  //     ]);
+  //     done();
+  //   }, 10);
+  // });
 });
