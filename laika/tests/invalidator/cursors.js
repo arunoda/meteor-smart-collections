36ebada2fb085b98072bea22ef96c21c740ac4d0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-27 00:24:14 +0530

    re-implemented cursor tests for invalidator

--- laika/tests/invalidator/cursors.js
+++ laika/tests/invalidator/cursors.js
@@ -4,90 +4,86 @@ require('../loader')('lib/deep_equal.js');
 require('../loader')('lib/invalidator.js');
 
 suite('Invalidator - cursor releated methods', function() {
-  // test('.addCursor() once', function() {
-  //   var invalidator = new Meteor.SmartInvalidator.constructor();
-  //   var cursor = {a: 10};
-  //   invalidator.addCursor('coll1', cursor);
-  //   assert.equal(invalidator._cursors['coll1'][0], cursor);
-  // });
+  test('.addCursor() once', function() {
+    var invalidator = new Meteor.SmartInvalidator();
+    var cursor = {a: 10};
+    invalidator.addCursor(cursor);
+    assert.equal(invalidator._cursors[0], cursor);
+  });
 
-  // test('.addCursor() twice', function() {
-  //   var invalidator = new Meteor.SmartInvalidator.constructor();
-  //   var cursor = {a: 10};
-  //   invalidator.addCursor('coll1', cursor);
-  //   invalidator.addCursor('coll1', cursor);
-  //   assert.equal(invalidator._cursors['coll1'][0], cursor);
-  //   assert.equal(invalidator._cursors['coll1'].length, 1);
-  // });
+  test('.addCursor() twice', function() {
+    var invalidator = new Meteor.SmartInvalidator();
+    var cursor = {a: 10};
+    invalidator.addCursor(cursor);
+    invalidator.addCursor(cursor);
+    assert.equal(invalidator._cursors[0], cursor);
+    assert.equal(invalidator._cursors.length, 1);
+  });
 
-  // test('.removeCursor()', function() {
-  //   var invalidator = new Meteor.SmartInvalidator.constructor();
-  //   var cursor = {a: 10};
-  //   invalidator._cursors['coll1'] = [cursor];
-  //   invalidator.removeCursor('coll1', cursor);
-  //   assert.equal(invalidator._cursors['coll1'].length, 0);
-  // });
+  test('.removeCursor()', function() {
+    var invalidator = new Meteor.SmartInvalidator();
+    var cursor = {a: 10};
+    invalidator._cursors = [cursor];
+    invalidator.removeCursor(cursor);
+    assert.equal(invalidator._cursors.length, 0);
+  });
 
   test('add cursor to the selector', function() {
-    var invalidator = new Meteor.SmartInvalidator.constructor();
+    var invalidator = new Meteor.SmartInvalidator();
     var cursor = {a: 10, _selector: {aa: 10}};
-    invalidator.addCursor('coll1', cursor);
+    invalidator.addCursor(cursor);
 
-    assert.deepEqual(invalidator._selectors, {'coll1': [
-      {selector: cursor._selector, cursors: [cursor]}
-    ]});
+    assert.deepEqual(invalidator._selectors, [{selector: cursor._selector, cursors: [cursor]}]);
   });
 
   test('add two cursors to the same selector', function() {
-    var invalidator = new Meteor.SmartInvalidator.constructor();
+    var invalidator = new Meteor.SmartInvalidator();
     var cursor = {a: 10, _selector: {aa: 10}};
     var cursor2 = {a: 11, _selector: {aa: 10}};
-    invalidator.addCursor('coll1', cursor);
-    invalidator.addCursor('coll1', cursor2);
+    invalidator.addCursor(cursor);
+    invalidator.addCursor(cursor2);
 
-    assert.deepEqual(invalidator._selectors, {'coll1': [
-      {selector: cursor._selector, cursors: [cursor, cursor2]}
-    ]});
+    assert.deepEqual(invalidator._selectors, [{selector: cursor._selector, cursors: [cursor, cursor2]}]);
   });
 
   test('add two cursors to two selectors', function() {
-    var invalidator = new Meteor.SmartInvalidator.constructor();
+    var invalidator = new Meteor.SmartInvalidator();
     var cursor = {a: 10, _selector: {aa: 10}};
     var cursor2 = {a: 11, _selector: {bb: 10}};
 
-    invalidator.addCursor('coll1', cursor);
-    invalidator.addCursor('coll1', cursor2);
+    invalidator.addCursor(cursor);
+    invalidator.addCursor(cursor2);
 
-    assert.deepEqual(invalidator._selectors, {'coll1': [
+    assert.deepEqual(invalidator._selectors, [
       {selector: cursor._selector, cursors: [cursor]},
       {selector: cursor2._selector, cursors: [cursor2]},
-    ]});
+    ]);
   });
 
   test('remove single cursor from a selector', function() {
-    var invalidator = new Meteor.SmartInvalidator.constructor();
+    var invalidator = new Meteor.SmartInvalidator();
     var cursor = {a: 10, _selector: {aa: 10}};
     var cursor2 = {a: 11, _selector: {aa: 10}};
-    invalidator.addCursor('coll1', cursor);
-    invalidator.addCursor('coll1', cursor2);
-    invalidator.removeCursor('coll1', cursor);
+    invalidator.addCursor(cursor);
+    invalidator.addCursor(cursor2);
+    invalidator.removeCursor(cursor);
 
-    assert.deepEqual(invalidator._selectors, {'coll1': [
+    assert.deepEqual(invalidator._selectors, [
       {selector: cursor._selector, cursors: [cursor2]}
-    ]});
+    ]);
   });
 
   test('remove cursor and the selector', function() {
-    var invalidator = new Meteor.SmartInvalidator.constructor();
+    var invalidator = new Meteor.SmartInvalidator();
     var cursor = {a: 10, _selector: {aa: 10}};
     var cursor2 = {a: 11, _selector: {bb: 10}};
 
-    invalidator.addCursor('coll1', cursor);
-    invalidator.addCursor('coll1', cursor2);
-    invalidator.removeCursor('coll1', cursor);
+    invalidator.addCursor(cursor);
+    invalidator.addCursor(cursor2);
+    invalidator.removeCursor(cursor);
 
-    assert.deepEqual(invalidator._selectors, {'coll1': [
+    assert.deepEqual(invalidator._selectors, [
       {selector: cursor2._selector, cursors: [cursor2]}
-    ]});
+    ]);
   });
 });
