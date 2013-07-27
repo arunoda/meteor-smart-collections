e681a0df53af3374ae8a954cdc22f79567ae665c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-27 07:24:40 +0530

    make server_collection compatible with new Queue Invalidator

--- package.js
+++ package.js
@@ -10,11 +10,10 @@ Package.on_use(function(api) {
   api.add_files([
     'lib/deep_equal.js',
     'lib/validator.js',
-    'lib/session_view.js',
-    'lib/version_manager.js',
     'lib/mongo.js',
     'lib/server_collection.js',
     'lib/invalidator.js',
+    'lib/op_queue.js',
     'lib/cursor.js',
     'lib/server_methods.js'
   ], 'server');
