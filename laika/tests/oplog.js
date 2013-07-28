0ca506db1626fa443629d1495edcca3c96740841
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-28 08:34:29 +0530

    added tests for oplog processor

---
+++ laika/tests/oplog.js
@@ -0,0 +1,111 @@
+var assert = require('assert');
+require('./loader')('lib/oplog.js');
+
+suite('Oplog Processor', function() {
+  test('insert', function(done) {
+    var op = {
+      "ns" : "db.aa",
+      "op": 'i',
+      "o" : {
+        "_id" : 'the-id',
+        "a" : 100
+      }
+    };
+
+    Meteor.SmartCollection = {
+      map: {
+        aa: {
+          opQueue: {
+            insert: function(doc) {
+              assert.deepEqual(doc, {_id: 'the-id', a: 100});
+              done();
+            }
+          }
+        }
+      }
+    };
+
+    Meteor.SmartOplog.processor(null, op);
+  });
+
+  test('update', function(done) {
+    var op = {
+      "ns" : "db.aa",
+      "op": 'u',
+      "o2" : {
+        "_id" : 'the-id'
+      },
+      "o" : {
+        "$set" : {
+          "b" : 300
+        }
+      }
+    };
+
+    Meteor.SmartCollection = {
+      map: {
+        aa: {
+          opQueue: {
+            update: function(id, modifier) {
+              assert.equal(id, 'the-id');
+              assert.deepEqual(modifier, {$set: {b: 300}});
+              done();
+            }
+          }
+        }
+      }
+    };
+
+    Meteor.SmartOplog.processor(null, op);
+  });
+
+  test('remove', function(done) {
+    var op = {
+      "ns" : "db.aa",
+      "op": 'd',
+      "o" : {
+        "_id" : 'the-id'
+      }
+    };
+
+    Meteor.SmartCollection = {
+      map: {
+        aa: {
+          opQueue: {
+            remove: function(id) {
+              assert.equal(id, 'the-id');
+              done();
+            }
+          }
+        }
+      }
+    };
+
+    Meteor.SmartOplog.processor(null, op);
+  });
+
+  test('drop', function(done) {
+    var op = {
+      "ns" : "db.$cmd",
+      "op": 'c',
+      "o" : {
+        "drop" : 'aa'
+      }
+    };
+
+    Meteor.SmartCollection = {
+      map: {
+        aa: {
+          opQueue: {
+            multiRemove: function(selector) {
+              assert.deepEqual(selector, {});
+              done();
+            }
+          }
+        }
+      }
+    };
+
+    Meteor.SmartOplog.processor(null, op);
+  });
+});
