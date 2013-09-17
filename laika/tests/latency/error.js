59fdb3011b5be71e0e9168c3b8cdd50a42307aeb
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-09-17 11:32:30 +0530

    removed Latency update error test, since the way now we are doing is unreliable

--- laika/tests/latency/error.js
+++ laika/tests/latency/error.js
@@ -55,68 +55,69 @@ suite('Latency - Error', function() {
     }, 300);
   });
 
-  test('update', function(done, server, client) {
-    server.evalSync(doAutoPublish);
-    server.evalSync(function() {
-      coll = new Meteor.SmartCollection('coll');
-      coll.insert({_id: 'aa', aa: "coola"});
-      coll._ensureIndex({aa: true});
+  //I couldn't able to find a reliable to make some error on update, so this test left blank
+  // test('update', function(done, server, client) {
+  //   server.evalSync(doAutoPublish);
+  //   server.evalSync(function() {
+  //     coll = new Meteor.SmartCollection('coll');
+  //     coll.insert({_id: 'aa', aa: "coola"});
+  //     coll._ensureIndex({aa: true});
 
-      coll.allow({
-        update: function() {return true}
-      })
+  //     coll.allow({
+  //       update: function() {return true}
+  //     })
 
-      //let indexes to happened properly
-      setTimeout(function() {
-        emit('return');
-      }, 200);
-    });
+  //     //let indexes to happened properly
+  //     setTimeout(function() {
+  //       emit('return');
+  //     }, 200);
+  //   });
 
-    var received = [];
-    var error;
-    client.on('added', function(doc) {
-      received.push(['a', doc])
-    });
+  //   var received = [];
+  //   var error;
+  //   client.on('added', function(doc) {
+  //     received.push(['a', doc])
+  //   });
 
-    client.on('removed', function(doc) {
-      received.push(['r', doc]);
-    });
+  //   client.on('removed', function(doc) {
+  //     received.push(['r', doc]);
+  //   });
 
-    client.on('changed', function(doc) {
-      received.push(['c', doc]);
-    });
+  //   client.on('changed', function(doc) {
+  //     received.push(['c', doc]);
+  //   });
 
-    client.on('error', function(err) {
-      error = err;
-    });
+  //   client.on('error', function(err) {
+  //     error = err;
+  //   });
 
-    client.eval(function() {
-      coll = new Meteor.SmartCollection('coll');
-      coll.find().observe({ 
-        added: function(doc) {
-          emit('added', doc);
-        },
-        removed: function(doc) {
-          emit('removed', doc);
-        },
-        changed: function(doc) {
-          emit('changed', doc);
-        }
-      });
+  //   client.eval(function() {
+  //     coll = new Meteor.SmartCollection('coll');
+  //     coll.find().observe({ 
+  //       added: function(doc) {
+  //         emit('added', doc);
+  //       },
+  //       removed: function(doc) {
+  //         emit('removed', doc);
+  //       },
+  //       changed: function(doc) {
+  //         emit('changed', doc);
+  //       }
+  //     });
 
-      coll.update({_id: 'aa'}, {$inc: {aa: 10}}, function(err) {
-        emit('error', err);
-      });
-    });
+  //     coll.update({_id: 'aa'}, {$inc: {aa: 10}}, function(err) {
+  //       emit('error', err);
+  //     });
+  //   });
 
-    setTimeout(function() {
-      assert.ok(error);
-      assert.deepEqual(received, [
-        ['a', {_id: 'aa', aa: "coola"}]
-      ]);
-      done();
-    }, 300);
-  });
+  //   setTimeout(function() {
+  //     assert.ok(error);
+  //     assert.deepEqual(received, [
+  //       ['a', {_id: 'aa', aa: "coola"}]
+  //     ]);
+  //     done();
+  //   }, 300);
+  // });
 
   //I couldn't able to find a way to make some error on remove, so this test left blank
   // test('remove', function(done, server, client) {
